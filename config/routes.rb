c39743be0755d4ad3d47d137e5aaee7c18e3490c
Andrew Eskenazi <Drewesk@gmail.com>
2016-10-25 13:29:51 -0600

    Updated config/routes.rb to fix merge-conflict
    [#132639119]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get 'time/now' => 'now#time'
     get 'introduce/:name1/and/:name2' => 'welcome#introduce'
 end
